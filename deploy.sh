362208d8a2aa518103ae877ebc7cb3abc6eb0f1d
ykarakita <taf.749@gmail.com>
2019-11-04 08:35:43 +0900

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ source $CONF
 rm -rf target
 
 mkdir target
-zip -r target/app.zip ./app
+zip -r target/app app LICENSE README.md
 
 sam package --template-file template.yml --output-template-file target/packaged.yml --s3-bucket sam-artifacts-524176662322-ap-northeast-1
 sam deploy --template-file target/packaged.yml --stack-name code-pipeline-slack-approver --capabilities CAPABILITY_IAM --parameter-overrides $PARAMS

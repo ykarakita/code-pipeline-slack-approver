b26a6d233502c915d92994cf43a8c01153dac772
ykarakita <taf.749@gmail.com>
2019-11-04 16:22:45 +0900

    Update file name

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ rm -rf target
 mkdir target
 zip -r target/app app LICENSE README.md
 
-sam package --template-file template.yml --output-template-file target/packaged.yml --s3-bucket sam-artifacts-524176662322-ap-northeast-1
-sam deploy --template-file target/packaged.yml --stack-name code-pipeline-slack-approver --capabilities CAPABILITY_IAM --parameter-overrides $PARAMS
+sam package --template-file template.yaml --output-template-file target/packaged.yaml --s3-bucket sam-artifacts-524176662322-ap-northeast-1
+sam deploy --template-file target/packaged.yaml --stack-name code-pipeline-slack-approver --capabilities CAPABILITY_IAM --parameter-overrides $PARAMS

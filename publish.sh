64ad61e50df0f83389e1d8d39457915e452e936c
ykarakita <taf.749@gmail.com>
2019-11-04 15:51:03 +0900

    Update publish region

--- publish.sh
+++ publish.sh
@@ -11,5 +11,5 @@ rm -rf target
 mkdir target
 zip -r target/app app LICENSE README.md
 
-sam package --region ap-northeast-1 --template-file template.yml --output-template-file target/packaged.yml --s3-bucket sam-artifacts-524176662322-ap-northeast-1
-sam publish --region ap-northeast-1 --template target/packaged.yml
+sam package --region us-east-1 --template-file template.yml --output-template-file target/packaged.yml --s3-bucket sam-artifacts-524176662322-us-east-1
+sam publish --region us-east-1 --template target/packaged.yml

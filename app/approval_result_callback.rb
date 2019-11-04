0a6da33b9a3cc2ac9fbb1ea4fe4dbe67df722bd4
ykarakita <taf.749@gmail.com>
2019-11-04 11:25:06 +0900

    Add ApprovalResultCallback Class

--- app/approval_result_callback.rb
+++ app/approval_result_callback.rb
@@ -3,60 +3,71 @@ require "aws-sdk-codepipeline"
 require "app/sender"
 
 def handler(event:, context:)
-  event_body = URI.decode(event["body"])
-  payload = JSON.parse(CGI.parse(event_body)["payload"].first)
-
-  response_to_user(payload)
-  pipeline_put_approval_result(payload)
+  approval_result_callback = ApprovalResultCallback.new(event, context)
+  approval_result_callback.response_to_user
+  approval_result_callback.pipeline_put_approval_result
 
   { statusCode: 200 }
 end
 
-def client
-  Aws::CodePipeline::Client.new
-end
+class ApprovalResultCallback
+  def initialize(event, context)
+    @event = event
+  end
 
-def response_to_user(payload)
-  action_value = action_value(payload)
-  username = username(payload)
+  def response_to_user
+    # Delete button from payload
+    payload["message"]["blocks"].delete_at(-1)
 
-  # Delete button from payload
-  payload["message"]["blocks"].delete_at(-1)
-
-  # Add action result to payload
-  payload["message"]["blocks"].push(
-    {
-      type: "section",
-      text: {
-        type: "mrkdwn",
-        text: "#{action_value["action"]} by #{username}"
+    # Add action result to payload
+    payload["message"]["blocks"].push(
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "#{action_value["action"]} by #{username}"
+        }
       }
-    }
-  )
-  sender = Sender.new(payload["response_url"])
-  sender.send!(payload["message"])
-end
+    )
+    sender = Sender.new(payload["response_url"])
+    sender.send!(payload["message"])
+  end
 
-def pipeline_put_approval_result(payload)
-  action_value = action_value(payload)
-  username = username(payload)
+  def pipeline_put_approval_result
+    client.put_approval_result(
+      {
+        pipeline_name: action_value["pipeline_name"],
+        stage_name: action_value["stage_name"],
+        action_name: action_value["action_name"],
+        result: {
+          summary: "#{action_value["action"]} by #{username}",
+          status: action_value["action"]
+        },
+        token: action_value["token"]
+      }
+    )
+  end
 
-  client.put_approval_result({
-    pipeline_name: action_value["pipeline_name"],
-    stage_name: action_value["stage_name"],
-    action_name: action_value["action_name"],
-    result: {
-      summary: "#{action_value["action"]} by #{username}",
-      status: action_value["action"]
-    },
-    token: action_value["token"]
-  })
-end
+  private
 
-def action_value(payload)
-  JSON.parse(payload["actions"].first["value"])
-end
+  attr_reader :event
 
-def username(payload)
-  payload["user"]["username"]
+  def payload
+    return @payload unless @payload.nil?
+
+    event_body = URI.decode(event["body"])
+    @payload = JSON.parse(CGI.parse(event_body)["payload"].first)
+  end
+
+  def username
+    payload["user"]["username"]
+  end
+
+  def action_value
+    @action_value ||= JSON.parse(payload["actions"].first["value"])
+  end
+
+  def client
+    @client ||= Aws::CodePipeline::Client.new
+  end
 end

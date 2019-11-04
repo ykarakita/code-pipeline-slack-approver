dd595799c604d0ac6f42969167c1728a3a3a328b
ykarakita <taf.749@gmail.com>
2019-11-04 11:08:47 +0900

    Separate functions

--- app/approval_result_callback.rb
+++ app/approval_result_callback.rb
@@ -6,23 +6,40 @@ def handler(event:, context:)
   event_body = URI.decode(event["body"])
   payload = JSON.parse(CGI.parse(event_body)["payload"].first)
 
-  username = payload["user"]["username"]
-  action_value = JSON.parse(payload["actions"].first["value"])
+  response_to_user(payload)
+  pipeline_put_approval_result(payload)
+
+  { statusCode: 200 }
+end
+
+def client
+  Aws::CodePipeline::Client.new
+end
+
+def response_to_user(payload)
+  action_value = action_value(payload)
+  username = username(payload)
 
   # Delete button from payload
   payload["message"]["blocks"].delete_at(-1)
 
   # Add action result to payload
-  payload["message"]["blocks"].push({
-    type: "section",
-    text: {
-      type: "mrkdwn",
-      text: "#{action_value["action"]} by #{username}"
+  payload["message"]["blocks"].push(
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: "#{action_value["action"]} by #{username}"
+      }
     }
-  })
-
+  )
   sender = Sender.new(payload["response_url"])
   sender.send!(payload["message"])
+end
+
+def pipeline_put_approval_result(payload)
+  action_value = action_value(payload)
+  username = username(payload)
 
   client.put_approval_result({
     pipeline_name: action_value["pipeline_name"],
@@ -34,10 +51,12 @@ def handler(event:, context:)
     },
     token: action_value["token"]
   })
-
-  { statusCode: 200 }
 end
 
-def client
-  Aws::CodePipeline::Client.new
+def action_value(payload)
+  JSON.parse(payload["actions"].first["value"])
+end
+
+def username(payload)
+  payload["user"]["username"]
 end
